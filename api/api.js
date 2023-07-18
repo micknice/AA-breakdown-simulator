a842ebb974727f0dccc8a21007c1d463537b85fd
mick nice <mikemccullagh@outlook.com>
2023-07-18 15:26:23 +0100

    env

--- api/api.js
+++ api/api.js
@@ -1,12 +1,16 @@
+const ENV = process.env.NODE_ENV || 'dev'
+
+require('dotenv').config({
+  path: `${__dirname}/../.env.${ENV}`,
+});
 const axios = require('axios');
 
 function getLatandLongByQuery(address, postCode) {
-    const apiKey = 'Ar9kKaB9YZmOVKisMev3y_uwZjSiP6UqlVwDSweBa4QZfpxnKr27cHzDWBy0Vg44';
+    const apiKey = process.env.BING_MAPS_API_KEY;
   
     return new Promise((resolve, reject) => {
       axios.get(`http://dev.virtualearth.net/REST/v1/Locations?countryRegion=uk&postalCode=${postCode}&addressLine=${address}&key=${apiKey}`)
         .then((response) => {
-          console.log('bing api response recieved!');
           const coordinates = response.data.resourceSets[0].resources[0].geocodePoints[0].coordinates;
           resolve(coordinates);
         })
@@ -18,14 +22,15 @@ function getLatandLongByQuery(address, postCode) {
   }
 
   function getDistanceAndTime(jobLoc, patrolLoc) {
-    console.log('get distance invoked');
-    const apiKey = 'Ar9kKaB9YZmOVKisMev3y_uwZjSiP6UqlVwDSweBa4QZfpxnKr27cHzDWBy0Vg44';
+    console.log('get distance invoked!!!!!!!');
+    console.log('api args!!!!!', jobLoc, patrolLoc)
+    const apiKey = process.env.BING_MAPS_API_KEY;
   
     return new Promise((resolve, reject) => {
       axios.get(`http://dev.virtualearth.net/REST/v1/Routes?wayPoint.1=${jobLoc}&wayPoint.2=${patrolLoc}&optimize=time&routeAttributes=routeSummariesOnly&maxSolutions=1&distanceUnit=Mile&key=${apiKey}`)
         .then((response) => {
           const resObj = response.data.resourceSets[0].resources[0];
-        //   console.log('distance @ api', response.data.resourceSets[0].resources[0]);
+        //DISTANCE IN MILES ETA IN SECONDS
           const result = {
             'distance': resObj.travelDistance,
             'eta': resObj.travelDuration,
@@ -39,7 +44,50 @@ function getLatandLongByQuery(address, postCode) {
         });
     });
   }
+  // function getDistanceAndTime(jobLoc, patrolLoc) {
+  //   console.log('get distance invoked!!!!!!!');
+  //   console.log('api args!!!!!', jobLoc, patrolLoc)
+  
+  //   return new Promise((resolve, reject) => {
+  //     axios.get(`http://dev.virtualearth.net/REST/v1/Routes?wayPoint.1=${jobLoc}&wayPoint.2=${patrolLoc}&optimize=time&routeAttributes=routeSummariesOnly&maxSolutions=1&distanceUnit=Mile&key=${apiKey}`)
+  //       .then((response) => {
+  //         const resObj = response.data.resourceSets[0].resources[0];
+  //       //   console.log('distance @ api', response.data.resourceSets[0].resources[0]);
+  //       //DISTANCE IN MILES ETA IN SECONDS
+  //         const result = {
+  //           'distance': resObj.travelDistance,
+  //           'eta': resObj.travelDuration,
+  //           'etaWithTraffic': resObj.travelDurationTraffic
+  //         };
+  //         resolve(result);
+  //       })
+  //       .catch((error) => {
+  //         console.log(error, 'error');
+  //         reject(error);
+  //       });
+  //   });
+  // }
+  function checkValidMainlandLocation(lat, long) {
+    console.log('!!!!!!process env', process.env.BING_MAPS_API_KEY)
+    const latLong = `${lat},${long}`
+    const apiKey = process.env.BING_MAPS_API_KEY;
+  
+    return new Promise((resolve, reject) => {
+      axios.get(`http://dev.virtualearth.net/REST/v1/locationrecog/${latLong}?key=${apiKey}&includeEntityTypes=address&output=json`)
+        .then((response) => {
+        //   resArr contains object with address details if valid latLong else empty arr
+          const resArr = response.data.resourceSets[0].resources[0].addressOfLocation
+          resolve(resArr);
+        })
+        .catch((error) => {
+          console.log(error, 'error');
+          reject(error);
+        });
+    });
+  }
 
 
-module.exports = {getLatandLongByQuery, getDistanceAndTime}
+  
+
+module.exports = {getLatandLongByQuery, getDistanceAndTime, checkValidMainlandLocation}
 
