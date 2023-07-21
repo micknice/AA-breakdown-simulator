044b99c263823290a2275bfb3d1803a4c214e211
mick nice <mikemccullagh@outlook.com>
2023-07-21 15:03:13 +0100

    adds scripts

--- broadcast/server.js
+++ broadcast/server.js
@@ -1,9 +1,9 @@
-const express = require('express');
+
 const http = require('http');
 const socketIO = require('socket.io');
 const Simulation = require('../simulation/runSimulation');
-const app = express();
-const server = http.createServer(app);
+
+const server = http.createServer();
 const io = socketIO(server, {cors: {origin: "*"}});
 
 const simulation = new Simulation();
@@ -18,10 +18,6 @@ simulation.startSimulation();
         io.emit('patrolData', simulation.getPatrolDataForGUI());
       }, 5000);
 
-
-app.get('/socket.io/socket.io.js', (req, res) => {
-  res.sendFile(__dirname + '/node_modules/socket.io-client/dist/socket.io.js');
-});
 
 const port = 7071; 
 server.listen(port, () => {
