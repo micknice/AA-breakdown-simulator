646542f40a0ed39e379bec62b9fdeb46ccf80f20
mick nice <mikemccullagh@outlook.com>
2023-10-10 06:37:39 +0100

    adds cors header

--- broadcast/server.js
+++ broadcast/server.js
@@ -8,6 +8,12 @@ const io = socketIO(server, { cors: { origin: "*" } });
 let simulation;
 let intervalId; 
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); 
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 io.on('connection', (socket) => {
   console.log('connected');
 
